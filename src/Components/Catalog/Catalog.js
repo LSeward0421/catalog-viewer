84cd26507ad662e41d93510efc9f0ad1633bb6ef
Lauren Seward <lauren.unal@gmail.com>
2023-08-10 19:47:43 -0500

    Remove duplicates and empty data

--- src/Components/Catalog/Catalog.js
+++ src/Components/Catalog/Catalog.js
@@ -1,14 +1,29 @@
-import './Catalog.css';
-import ProductCard from '../ProductCard/ProductCard';
+import "./Catalog.css";
+import ProductCard from "../ProductCard/ProductCard";
 
 const Catalog = ({ data }) => {
+  const skuSet = new Set();
+  const uniqueProducts = data.products.filter((product) => {
+    if (!skuSet.has(product.sku)) {
+      skuSet.add(product.sku);
+      return true;
+    }
+    return false;
+  });
+  const filteredProducts = uniqueProducts.filter(
+    (product) =>
+      product.name && product.author && product.images && product.images[0]
+  );
+  const sortedProducts = filteredProducts.sort((a, b) =>
+    a.name > b.name ? 1 : -1
+  );
   return (
-    <div className='catalog'>
-      {data.map((product, index) => (
-        <ProductCard key={index} product={product} />
+    <div className="catalog">
+      {sortedProducts.map(({ sku, ...product }) => (
+        <ProductCard key={sku} product={product} />
       ))}
     </div>
-  )
-}
+  );
+};
 
 export default Catalog;
--- src/Components/ProductCard/ProductCard.js
+++ src/Components/ProductCard/ProductCard.js
@@ -1,18 +1,18 @@
-import './ProductCard.css';
+import "./ProductCard.css";
 
 const ProductCard = ({ product }) => {
   const { name, subName, author, images } = product;
-  const imageURL = images && images[0] ? images[0].uri : '';
-  const formatAuthor = author ? author.split(', ').reverse().join(' ') : '';
+  const imageURL = images && images[0] ? images[0].uri : "";
+  const formatAuthor = author ? author.split(", ").reverse().join(" ") : "";
 
   return (
-    <div className='product-card'>
+    <div className="product-card">
       <img src={imageURL} alt={name} />
       <h2>{name}</h2>
       {subName && <h3>{subName}</h3>}
-      <p>{formatAuthor}</p>
+      <p>Author: {formatAuthor}</p>
     </div>
-  )
-}
+  );
+};
 
 export default ProductCard;
